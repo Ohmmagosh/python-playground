6c143dbf3588e5c8c23d022b4cf5fb89b358a8fa
ohmmaagoch <ohm@42bangkok.com>
2024-02-13 10:15:19 +0700

    Remove unnecessary imports and variables

--- profit-calculator/app/main.py
+++ profit-calculator/app/main.py
@@ -1,12 +1,4 @@
-from time import sleep
-from random import randint
-from rich.console import Console
-from rich.table import Table
-
 from app import App
-
-profit = [0.03, 0.05, 0.1]
-console = Console()
 
 def main():
     App().run()
